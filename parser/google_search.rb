dd9878182f19cab380b345044f05efcf7f266650
pwener <phwener@gmail.com>
2017-06-29 10:50:37 -0300

    Removing google sample

--- parser/google_search.rb
+++
@@ -1,9 +0,0 @@
-require 'open-uri'
-require 'nokogiri'
-
-page = open "http://www.google.com/search?num=100&q=stackoverflow"
-html = Nokogiri::HTML page
-
-html.search("cite").each do |cite|
-  puts cite.inner_text
-end
