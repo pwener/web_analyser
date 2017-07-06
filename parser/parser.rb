8b2c8f301182bb8dcfb839935ea4857cb3cc8bf2
alexandretk <alexandrekry@gmail.com>
2017-07-06 16:11:31 -0300

    Updating parser... Parsing many google pages

--- parser/parser.rb
+++ parser/parser.rb
@@ -3,26 +3,45 @@ require './page'
 
 # Inject target subject into specific search engine url
 target = ARGV[0]
-
+total_pages = ARGV[1]
 # search_engines = Array.new
 # search_engines.push("https://duckduckgo.com/?q=#{target}")
-url = "http://www.google.com/search?q=#{target}"
 
-visitor = AutoVisitor.new(url)
-visitor.search_links()
+# PARAMS
+# search?q=
+# &num=
+# &start=
+# url = "http://www.google.com/search?q=#{target}"
 
-puts "Found #{visitor.urls.size} urls"
+pages_parsed = 0
+page = 0
+begin
+	page = page + 1
+	linksPerPage = 10
+	url = "http://www.google.com/search?q=#{target}&num=#{linksPerPage}&start=#{(linksPerPage)*(page -1)}"
+	puts url
+	visitor = AutoVisitor.new(url)
+	visitor.search_links()
 
-visitor.urls.each do |url|
-  # URL needs one specific pattern to be valid to the nokogiri
-	encoded_url = URI.encode(url)
-	final_uri = URI.parse(encoded_url).to_s
+	puts "Found #{visitor.urls.size} urls"
 
-	begin
-		p = Page.new(final_uri)
-		p.printf
-	rescue OpenURI::HTTPError, SocketError
-		puts "#{final_uri} has fails"
+	visitor.urls.each do |url|
+	  # URL needs one specific pattern to be valid to the nokogiri
+		encoded_url = URI.encode(url)
+		final_uri = URI.parse(encoded_url).to_s
+
+		begin
+			p = Page.new(final_uri)
+			p.printf
+		rescue OpenURI::HTTPError, SocketError
+			puts "#{final_uri} has failed"
+		end
 	end
 
-end
+
+	pages_parsed = pages_parsed + visitor.urls.size
+	if pages_parsed >= total_pages.to_i
+		break
+	end
+
+end while (visitor.urls.size > 0)
