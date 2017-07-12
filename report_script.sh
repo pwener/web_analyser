bbe939cd43f0a64accec4c380f356d38a058e008
pwener <phwener@gmail.com>
2017-07-12 13:03:15 -0300

    Fixing report script

--- report_script.sh
+++ report_script.sh
@@ -4,11 +4,9 @@ target=final_report.md
 
 echo -n > $target
 
-#parts="introduction hdfs map_reduce yarn hadoop_install hadoop_simple \
-#			parser word_count hadoop_stress_test hadoop_stress_test_2 simple_word_counter conclusion"
 
 parts="introduction contexto parser hdfs map_reduce yarn hadoop_install hadoop_simple \
-		 	word_count all_tests simple_word_counter conclusion"
+				word_count all_tests simple_word_counter conclusion"
 
 
 for part in $parts; do
